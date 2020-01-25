0c5280a3e15014a02eff5415ce37b078b08945f5
Fabian Thomas <fabithomi@web.de>
2020-01-26 00:01:41 +0100

    completely reworked the control (Nuget 1.1.3)

--- WpfCropableImageControl/CropableImage.xaml.cs
+++ WpfCropableImageControl/CropableImage.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -45,6 +47,120 @@ namespace WpfCropableImageControl
 
         #endregion
 
+        #region CropHeight
+
+        // the desired CropHeight (from User)
+        public static DependencyProperty CropHeightProperty = DependencyProperty.Register(nameof(CropHeight), typeof(int?), typeof(CropableImage), new PropertyMetadata(CropHeight_Changed));
+        public int? CropHeight
+        {
+            get => (int?)GetValue(CropHeightProperty);
+            set => SetValue(CropHeightProperty, value);
+        }
+        private static void CropHeight_Changed(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (sender is CropableImage cropableImage && e.OldValue != e.NewValue)
+                cropableImage.UpdateHeight();
+        }
+
+        private void UpdateHeight()
+        {
+            if (CropHeight.HasValue)
+            {
+                if (CropHeight.Value > BigImageHeight - ShiftY)
+                {
+                    if (BigImageHeight - ShiftY >= 0)
+                        ActualCropHeight = BigImageHeight - ShiftY;
+                    else
+                        ActualCropHeight = 0;
+                }
+                else if (CropHeight.Value < 0)
+                    ActualCropHeight = 0;
+                else
+                    ActualCropHeight = CropHeight.Value;
+            }
+            else
+            {
+                if (BigImageHeight - ShiftY >= 0)
+                    ActualCropHeight = BigImageHeight - ShiftY;
+                else
+                    ActualCropHeight = 0;
+            }
+        }
+
+        // the actual verified CropHeight
+        private int _actualCropHeight;
+        public int ActualCropHeight
+        {
+            get => _actualCropHeight;
+            private set
+            {
+                if (value != _actualCropHeight)
+                {
+                    _actualCropHeight = value;
+                    OnPropertyChanged("ActualCropHeight");
+                }
+            }
+        }
+
+        #endregion
+
+        #region CropWidth
+
+        // the desired CropWidth (from User)
+        public static DependencyProperty CropWidthProperty = DependencyProperty.Register(nameof(CropWidth), typeof(int?), typeof(CropableImage), new PropertyMetadata(CropWidth_Changed));
+        public int? CropWidth
+        {
+            get => (int?)GetValue(CropWidthProperty);
+            set => SetValue(CropWidthProperty, value);
+        }
+        private static void CropWidth_Changed(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (sender is CropableImage cropableImage && e.OldValue != e.NewValue)
+                cropableImage.UpdateWidth();
+        }
+
+        private void UpdateWidth()
+        {
+            if (CropWidth.HasValue)
+            {
+                if (CropWidth.Value > BigImageWidth - ShiftX)
+                {
+                    if (BigImageWidth - ShiftX >= 0)
+                        ActualCropWidth = BigImageWidth - ShiftX;
+                    else
+                        ActualCropWidth = 0;
+                }
+                else if (CropWidth.Value < 0)
+                    ActualCropWidth = 0;
+                else
+                    ActualCropWidth = CropWidth.Value;
+            }
+            else
+            {
+                if (BigImageWidth - ShiftX >= 0)
+                    ActualCropWidth = BigImageWidth - ShiftX;
+                else
+                    ActualCropWidth = 0;
+            }
+        }
+
+        // the actual verified CropWidth
+        private int _actualCropWidth;
+        public int ActualCropWidth
+        {
+            get => _actualCropWidth;
+            private set
+            {
+                if (value != _actualCropWidth)
+                {
+                    _actualCropWidth = value;
+                    OnPropertyChanged("ActualCropWidth");
+                }
+            }
+        }
+
+        #endregion
+
         #region ShiftY
 
         public static DependencyProperty ShiftYProperty = DependencyProperty.Register(nameof(ShiftY), typeof(int), typeof(CropableImage), new PropertyMetadata(0, ShiftY_Changed));
@@ -58,39 +174,7 @@ namespace WpfCropableImageControl
             if (sender is CropableImage cropableImage && e.OldValue != e.NewValue)
             {
                 (cropableImage.MainGrid.RenderTransform as TranslateTransform).Y = -(int)e.NewValue;
-                cropableImage.CropHeight_Verify();
-            }
-        }
-
-        #endregion
-
-        #region CropHeight
-
-        // the desired CropHeight (from User)
-        public static DependencyProperty CropHeightProperty = DependencyProperty.Register(nameof(CropHeight), typeof(int), typeof(CropableImage), new PropertyMetadata(-1, CropHeight_Changed));
-        public int CropHeight
-        {
-            get => (int)GetValue(CropHeightProperty);
-            set => SetValue(CropHeightProperty, value);
-        }
-        private static void CropHeight_Changed(object sender, DependencyPropertyChangedEventArgs e)
-        {
-            if (sender is CropableImage cropableImage && e.OldValue != e.NewValue)
-                cropableImage.CropHeight_Verify();
-        }
-
-        // the actual verified CropHeight
-        private int _actualCropHeight;
-        public int ActualCropHeight
-        {
-            get => _actualCropHeight;
-            set
-            {
-                if (value != _actualCropHeight)
-                {
-                    _actualCropHeight = value;
-                    OnPropertyChanged("ActualCropHeight");
-                }
+                cropableImage.UpdateHeight();
             }
         }
 
@@ -109,51 +193,31 @@ namespace WpfCropableImageControl
             if (sender is CropableImage cropableImage && e.OldValue != e.NewValue)
             {
                 (cropableImage.MainGrid.RenderTransform as TranslateTransform).X = -(int)e.NewValue;
-                cropableImage.CropWidth_Verify();
+                cropableImage.UpdateWidth();
             }
         }
 
         #endregion
 
-        #region CropWidth
-
-        // the desired CropWidth (from User)
-        public static DependencyProperty CropWidthProperty = DependencyProperty.Register(nameof(CropWidth), typeof(int), typeof(CropableImage), new PropertyMetadata(-1, CropWidth_Changed));
-        public int CropWidth
+        private int _bigImageHeight;
+        public int BigImageHeight
         {
-            get => (int)GetValue(CropWidthProperty);
-            set => SetValue(CropWidthProperty, value);
-        }
-        private static void CropWidth_Changed(object sender, DependencyPropertyChangedEventArgs e)
-        {
-            if (sender is CropableImage cropableImage && e.OldValue != e.NewValue)
-                cropableImage.CropWidth_Verify();
-        }
-
-        // the actual verified CropHeight
-        private int _actualCropWidth;
-        public int ActualCropWidth
-        {
-            get => _actualCropWidth;
-            set
+            get => _bigImageHeight;
+            private set
             {
-                if (value != _actualCropWidth)
+                if (value != _bigImageHeight)
                 {
-                    _actualCropWidth = value;
-                    OnPropertyChanged("ActualCropWidth");
+                    _bigImageHeight = value;
+                    OnPropertyChanged("BigImageHeight");
                 }
             }
         }
-
-        #endregion
-
-        private int BigImageHeight;
 
         private int _bigImageWidth;
         public int BigImageWidth
         {
             get => _bigImageWidth;
-            set
+            private set
             {
                 if (value != _bigImageWidth)
                 {
@@ -170,44 +234,10 @@ namespace WpfCropableImageControl
 
         #endregion
 
-        #region Verifiers
-
-        private void CropHeight_Verify()
-        {
-            if (CropHeight != -1)
-            {
-                if (CropHeight > BigImageHeight - ShiftY)
-                    ActualCropHeight = BigImageHeight - ShiftY;
-                else if (CropHeight < 0)
-                    ActualCropHeight = 0;
-                else
-                    ActualCropHeight = CropHeight;
-            }
-            else
-                ActualCropHeight = BigImageHeight - ShiftY;
-        }
-
-        private void CropWidth_Verify()
-        {
-            if (CropWidth != -1)
-            {
-                if (CropWidth > BigImageWidth - ShiftX)
-                    ActualCropWidth = BigImageWidth - ShiftX;
-                else if (CropWidth < 0)
-                    ActualCropWidth = 0;
-                else
-                    ActualCropWidth = CropWidth;
-            }
-            else
-                ActualCropWidth = BigImageWidth - ShiftX;
-        }
-
-        #endregion
-
         public CropableImage()
         {
-            DataContext = this;
             InitializeComponent();
+            MainViewBox.DataContext = this;
 
             MainGrid.RenderTransform = new TranslateTransform();
         }
@@ -224,9 +254,9 @@ namespace WpfCropableImageControl
                 var pixelWidth = 0;
                 foreach (var b in Images)
                 {
-                    var image = b as ImageSource;
-                    pixelHeight += (int)Math.Round(image.Height);
-                    var width = (int)Math.Round(image.Width);
+                    var image = b as BitmapSource;
+                    pixelHeight += image.PixelHeight;
+                    var width = image.PixelWidth;
                     if (pixelWidth < width)
                         pixelWidth = width;
                 }
@@ -293,22 +323,6 @@ namespace WpfCropableImageControl
                     }
                 }
 
-                var endHeight = 0;
-                foreach (var r in horizontalImageParts[0])
-                    endHeight += r.Height;
-
-                var endWidth = 0;
-                foreach (var c in horizontalImageParts)
-                    endWidth += c[0].Width;
-
-                BigImageHeight = endHeight;
-                if (CropHeight == -1)
-                    ActualCropHeight = BigImageHeight;
-
-                BigImageWidth = endWidth;
-                if (CropWidth == -1)
-                    ActualCropWidth = BigImageWidth;
-
                 // build final image as imagegrid
                 foreach (var column in horizontalImageParts)
                 {
@@ -323,32 +337,36 @@ namespace WpfCropableImageControl
                         row++;
                     }
                 }
+
+                var endHeight = 0;
+                foreach (var r in horizontalImageParts[0])
+                    endHeight += r.Height;
+
+                var endWidth = 0;
+                foreach (var c in horizontalImageParts)
+                    endWidth += c[0].Width;
+
+                // set height/width if no cropheight/cropwidth is specified
+                BigImageHeight = endHeight;
+                UpdateHeight();
+                BigImageWidth = endWidth;
+                UpdateWidth();
             }
         }
 
         private void InitializeBigImage(int row, int column, ImagePart imagePart)
         {
-            var s = new StackPanel() { Width = imagePart.Width };
+            var viewBox = new Viewbox { Width = imagePart.Width, Height = imagePart.Height, Stretch = Stretch.None, HorizontalAlignment = HorizontalAlignment.Left };
 
-            //var st = new UserControl() {  };
+            var stackPanel = new StackPanel();
+            foreach (var b in Images as BitmapSource[])
+                stackPanel.Children.Add(new Image() { Source = b as BitmapSource, Width= b.PixelWidth, Height=b.PixelHeight });
+            stackPanel.RenderTransform = new TranslateTransform() { Y = -imagePart.Y, X = -imagePart.X };
 
-            var shiftStackPanel = new StackPanel();
-            foreach (var b in Images)
-                shiftStackPanel.Children.Add(new Image() { Source = b as BitmapSource });
-
-            var sizeStackPanel = new StackPanel() { Height = imagePart.Height, Width = 100 };
-            sizeStackPanel.Children.Add(shiftStackPanel);
-
-            //st.Child = sizeStackPanel;
-            //st.Content = sizeStackPanel;
-            s.Children.Add(sizeStackPanel);
-            //s.Content = st;
-
-            MainGrid.Children.Add(s);
-            Grid.SetRow(s, row);
-            Grid.SetColumn(s, column);
-
-            shiftStackPanel.RenderTransform = new TranslateTransform() { Y = -imagePart.Y, X = -imagePart.X };
+            viewBox.Child = stackPanel;
+            MainGrid.Children.Add(viewBox);
+            Grid.SetRow(viewBox, row);
+            Grid.SetColumn(viewBox, column);
         }
 
         private class ImagePart
